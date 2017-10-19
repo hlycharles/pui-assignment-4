ae3b51838de93b7099e3c6dac9743bedb4e18a71
Luyao Hou <hly.charles@gmail.com>
2017-10-19 15:44:33 -0400

    setup overlay component

--- js/detail.js
+++ js/detail.js
@@ -1,18 +1,19 @@
 window.onload = function() {
+    // constants
+    const availableQuantities = [];
+    for (let i = 1; i <= 10; i++) {
+        availableQuantities.push(i);
+    }
+    const overlayColNum = 3;
+
     // current number of items to add to cart
     let purchaseNum = 1;
     // current number of items in the cart
     let cartCount = 0;
-
-    // register onclick events for purchase number operations
-    const incBtns = document.getElementsByClassName("num-inc-btn");
-    for (let i = 0; i < incBtns.length; i++) {
-        incBtns[i].onclick = incPurchaseNum;
-    }
-    const decBtns = document.getElementsByClassName("num-dec-btn");
-    for (let i = 0; i < incBtns.length; i++) {
-        decBtns[i].onclick = decPurchaseNum;
-    }
+    // show quantity selection
+    let showQuantitySelection = false;
+    // show falvor selection
+    let showFlavorSelection = false;
 
     // register onclick events for adding items to cart
     const cartBtns = document.getElementsByClassName("cart-btn");
@@ -20,19 +21,19 @@ window.onload = function() {
         cartBtns[i].onclick = addToCart;
     }
 
+    // register onclick events for quantity selection
+    const quantityBtns = document.getElementsByClassName("pack-quantity-btn");
+    for (let i = 0; i < quantityBtns.length; i++) {
+        quantityBtns[i].onclick = selectQuantity;
+    }
+
+    // register onclick events for flavor selection
+    const flavorBtns = document.getElementsByClassName("add-flavor-btn");
+    for (let i = 0; i < flavorBtns.length; i++) {
+        flavorBtns[i].onclick = selectFlavor;
+    }
+
     render();
-
-    function incPurchaseNum() {
-        purchaseNum++;
-        render();
-    }
-
-    function decPurchaseNum() {
-        if (purchaseNum > 1) {
-            purchaseNum--;
-            render();
-        }
-    }
 
     function addToCart() {
         cartCount += purchaseNum;
@@ -40,11 +41,80 @@ window.onload = function() {
         render();
     }
 
+    function selectQuantity() {
+        showQuantitySelection = true;
+        render();
+    }
+
+    function selectFlavor() {
+        showFlavorSelection = true;
+        render();
+    }
+
+    function createOverlay(title, items, colNum, isSparse) {
+        const overlayElem = document.createElement("div");
+        overlayElem.classList.add("overlay");
+        const titleContainer = document.createElement("div");
+        titleContainer.classList.add("overlay-row");
+        if (isSparse) {
+            titleContainer.classList.add("overlay-row-shrt");
+        }
+        const titleElem = document.createElement("h5");
+        const titleText = document.createTextNode(title);
+        titleElem.appendChild(titleText);
+        titleContainer.appendChild(titleElem);
+        overlayElem.appendChild(titleContainer);
+        for (let i = 0; i < items.length; i += colNum) {
+            const rowElem = document.createElement("div");
+            rowElem.classList.add("overlay-row");
+            if (isSparse) {
+                rowElem.classList.add("overlay-row-shrt");
+            }
+            for (let j = i; j < i + colNum && j < items.length; j++) {
+                const btnElem = document.createElement("button");
+                btnElem.classList.add("overlay-btn")
+                if (isSparse) {
+                    btnElem.classList.add("overlay-btn-sm");
+                }
+                btnElem.classList.add(isSparse ? "btn-secondary" : "btn-tertiary");
+                btnElem.innerHTML = items[j].name;
+                rowElem.appendChild(btnElem);
+            }
+            overlayElem.appendChild(rowElem);
+        }
+        document.body.appendChild(overlayElem);
+    }
+
     function render() {
-        // update currently selected purchase number
-        const numElems = document.getElementsByClassName("purchase-num");
-        for (let i = 0; i < numElems.length; i++) {
-            numElems[i].innerHTML = purchaseNum.toString();
+
+        if (showQuantitySelection) {
+            quantities = [];
+            for (let i = 0; i < availableQuantities.length; i++) {
+                quantities.push({
+                    name: availableQuantities[i].toString(),
+                });
+            }
+            createOverlay("Quantity", quantities, overlayColNum, true);
+        }
+
+        if (showFlavorSelection) {
+            flavors = [];
+            for (const k in Roll) {
+                if (Roll.hasOwnProperty(k)) {
+                    const roll = Roll[k];
+                    let name = roll.name;
+                    if (!!roll.isGlutenFree) {
+                        name += " (Gluten-Free)";
+                    }
+                    if (!!roll.isVegan) {
+                        name += " (Vegan)";
+                    }
+                    flavors.push({
+                        name: name,
+                    });
+                }
+            }
+            createOverlay("Flavor", flavors, overlayColNum, false);
         }
 
         // update shopping cart
